dc61e4e47949f2428a918b798db84e450a06c80f
Lapto-Vu <smallcounter@gmail.com>
2021-06-07 22:03:22 +0900

    updated

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const pageOneClick = document.querySelector(".js-page-one-click")
 const pageTwoClick = document.querySelector(".js-page-two-click")
+const pageThreeClick = document.querySelector(".js-page-three-click")
 
 const pageOne = document.querySelector(".js-page-one")
 const pageTwo = document.querySelector(".js-page-two")
-
+const pageThree = document.querySelector(".js-page-three")
 
 function pageDown(e) {
     e.style.zIndex="-1";
@@ -17,16 +18,25 @@ function pageUp(e) {
     e.style.opacity="1";
 }
 
+function handlePageThree () {
+    pageUp(pageThree);
+    pageDown(pageTwo);
+    pageDown(pageOne);
+}
+
+
 function handlePageTwo () {
     pageUp(pageTwo);
+    pageDown(pageThree);
     pageDown(pageOne);
 }
 
 function handlePageOne () {
     pageUp(pageOne);
+    pageDown(pageThree);
     pageDown(pageTwo);
 }
 
-
-pageTwoClick.addEventListener("click", handlePageTwo);
 pageOneClick.addEventListener("click", handlePageOne);
+pageTwoClick.addEventListener("click", handlePageTwo);
+pageThreeClick.addEventListener("click", handlePageThree);
